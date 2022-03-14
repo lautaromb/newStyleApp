49eb07e56d5b0aa304c690edfa59b1b807c042d9
Fermin_Colorado <fermin.ciro@esumer.edu.co>
2022-03-13 21:47:57 -0500

    Vue login&register

--- src/main/resources/static/web/js/home.js
+++ src/main/resources/static/web/js/home.js
@@ -1,7 +1,29 @@
 var app = new Vue({
-	el: "#app",
-	data: {
+    el: "#app",
+    data: {
+        email: "",
+        password: ""
 
-		
-	}
+    },
+
+
+    methods: {
+        signIn() {
+            axios.post('/api/login', "email=" + this.email + "&password=" + this.password, { headers: { 'content-type': 'application/x-www-form-urlencoded' } })
+                .then(response => { window.location.href = "/web/home.html" })
+                .catch(error => {
+
+                    Swal.fire({
+                        title: 'Invalid combination',
+                        text: 'Please re-enter your details',
+                        icon: "error",
+                        showConfirmButton: false,
+                        timer: 2000,
+                    })
+
+                })
+
+        },
+
+    }
 })
--- src/main/resources/static/web/js/index.js
+++ src/main/resources/static/web/js/index.js
@@ -1,6 +1,62 @@
 var app = new Vue({
-  el: "#app",
-  data: {
-      
-  },
-});
+    el: "#app",
+    data: {
+        primerNombre: '',
+        apellido: '',
+        email: '',
+        password: '',
+        numeroTelefono: ''
+
+    },
+
+
+    methods: {
+        iniciarSesión() {
+            axios.post('/api/login', "email=" + this.email + "&password=" + this.password, { headers: { 'content-type': 'application/x-www-form-urlencoded' } })
+                .then(response => { window.location.href = "/web/home.html" })
+                .catch(error => {
+
+                    Swal.fire({
+                        title: 'Invalid combination',
+                        text: 'Please re-enter your details',
+                        icon: "error",
+                        showConfirmButton: false,
+                        timer: 2000,
+                    })
+                })
+        },
+
+        registrarse() {
+            if (this.primerNombre != '' && this.apellido != '' && this.email != '' && this.password != '' && this.numeroTelefono != '') {
+                Swal.fire({
+                    title: 'REGISTERED!',
+                    text: `Welcome ${this.primerNombre.toUpperCase()} ${this.apellido.toUpperCase()}!`,
+                    icon: "success",
+                    showConfirmButton: false,
+                    timer: 2500,
+
+                })
+                setTimeout(() => {
+                    axios.post('/api/clientes', "primerNombre=" + this.primerNombre + "&apellido=" + this.apellido + "&email=" + this.email + "&password=" + this.password + "&numeroTel=" + this.numeroTelefono, { headers: { 'content-type': 'application/x-www-form-urlencoded' } })
+                        .then(response => {
+
+                            this.iniciarSesión()
+                        })
+                        .catch(error => console.log(`Error en: ${error}`))
+                }, 2000);
+            } else {
+                Swal.fire({
+                    title: 'Please fill in all the spaces',
+                    // text: 'Please fill in all the spaces',
+                    icon: "error",
+                    showConfirmButton: false,
+                    timer: 3000,
+                })
+
+            }
+
+
+        },
+
+    }
+})
