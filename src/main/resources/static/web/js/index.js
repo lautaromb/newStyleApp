7102d4333e57888a2ba83f4673292ec88750a9c7
Claudio_Pactat <claudio.pactat@gmail.com>
2022-03-16 15:37:56 -0300

    modificados todos los formularios, inicio de seccion arreglado, accesos admin y clientes diferenciados

--- src/main/resources/static/web/js/index.js
+++ src/main/resources/static/web/js/index.js
@@ -10,11 +10,39 @@ var app = new Vue({
 
 
     methods: {
+      iniciarSesion() {
+        axios.post('/api/login', "email=" + this.email + "&password=" + this.password, { headers: { 'content-type': 'application/x-www-form-urlencoded' } })
+            .then(response => { window.location.href = "/web/html/home.html" })
+            .catch(error => {
+
+                Swal.fire({
+                    title: 'Error',
+                    text: 'Email o contraseña incorrecta. Vuelve a intentarlo.',
+                    icon: "error",
+                    showConfirmButton: false,
+                    timer: 2200,
+                })
+            })
+    },
+      /*
         iniciarSesion() {
             axios.post('/api/login', "email=" + this.email + "&password=" + this.password, { headers: { 'content-type': 'application/x-www-form-urlencoded' } })
-                .then(response => { window.location.href = "/web/html/home.html" })
-                .catch(error => {
+                
+                .then(response => {
+                  if(this.email.includes("@") && this.password != ""){
+                      Swal.fire({
+                          icon: 'success',
+                          title: 'Correcto',
+                          text: 'Bienvenido!!',
+                          showConfirmButton: false,
+                      })
+                  }
+                  setTimeout(() => {
+                      window.location.href = '/web/html/home.html'
+                  }, 2000);
+                })
 
+                .catch(error => {
                     Swal.fire({
                         title: 'Error',
                         text: 'Email o contraseña incorrecta. Vuelve a intentarlo.',
@@ -24,40 +52,41 @@ var app = new Vue({
                     })
                 })
         },
+        */
         registrarse() {
-            if(this.primerNombre != '' && this.apellido != '' && this.email.includes("@") != '' && this.password != '' && this.numeroTelefono){
-              Swal.fire({
-                icon: 'success',
-                title: 'Te registraste correctamente!',
-                showConfirmButton: false,
-                timer: 2000
-              })
-              setTimeout(() => {
-                axios
-                .post(
-                  "/api/clientes",
-                  `primerNombre=${this.primerNombre}&apellido=${this.apellido}&email=${this.email}&password=${this.password}&numeroTel=${this.numeroTelefono}`
-                )
-                .then((response) => {
-                    console.log("registered");
-                    
-                  this.iniciarSesion()
-      
-                  })
-        
-                .catch((e) => {
-                  console.log(e);
-                });
-              }, 2100);
-            } else {
-              Swal.fire({
-                text: 'Por favor completa los datos anteriores correctamente',
-                icon: 'error',
-                showConfirmButton: false,
-                timer: 2500
-              })
-            }  
-            
+          if(this.primerNombre != '' && this.apellido != '' && this.email.includes("@") != '' && this.password != '' && this.numeroTelefono){
+            Swal.fire({
+              icon: 'success',
+              title: 'Te registraste correctamente!',
+              showConfirmButton: false,
+              timer: 2000
+            })
+            setTimeout(() => {
+              axios
+              .post(
+                "/api/clientes",
+                `primerNombre=${this.primerNombre}&apellido=${this.apellido}&email=${this.email}&password=${this.password}&numeroTel=${this.numeroTelefono}`
+              )
+              .then((response) => {
+                  console.log("registered");
+
+                this.iniciarSesion()
+
+                })
+
+              .catch((e) => {
+                console.log(e);
+              });
+            }, 2100);
+          } else {
+            Swal.fire({
+              text: 'Por favor completa los datos anteriores correctamente',
+              icon: 'error',
+              showConfirmButton: false,
+              timer: 2500
+            })
           }
+
+        }
 }
 })
