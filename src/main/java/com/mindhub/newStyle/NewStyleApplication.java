07047229d790ed7f4a90526084220b177ce7a4b1
Matias_Domato <matiasdomato97@gmail.com>
2022-03-16 19:00:51 -0300

    commit mati carrito

--- src/main/java/com/mindhub/newStyle/NewStyleApplication.java
+++ src/main/java/com/mindhub/newStyle/NewStyleApplication.java
@@ -39,28 +39,28 @@ public class NewStyleApplication {
 			Negocio newStyle = new Negocio("New Style", "newStyle@gmail.com", "Av. de Mayo 2859");
 
 
-			Servicio servicioCorteDePelo = new Servicio("Peluquería", 400.0, "https://i.imgur.com/P3HUPFc.jpeg", "url aqui","Corte Lavado Enguaje", newStyle);
+			Servicio servicioCorteDePelo = new Servicio("Corte de pelo", 400.0, "https://i.imgur.com/P3HUPFc.jpeg", "url aqui","Corte a elección del cliente", newStyle);
 			Servicio servicioTintura = new Servicio("Tintura", 700.0, "url", "url", "tintura", newStyle);
 			Servicio servicioAlisado = new Servicio("Alisado", 1100.0, "url aqui", "url aqui","Tratamiento de alisado", newStyle);
-			Servicio servicioBarberia = new Servicio("Barbería", 200.0,  "url aqui", "url aqui","Corte Aceitado", newStyle);
+			Servicio servicioBarberia = new Servicio("Barba", 200.0,  "url aqui", "url aqui","Corte Aceitado", newStyle);
 			Servicio servicioManicura = new Servicio("Manicura", 500.0,  "url aqui", "url aqui","Limpieza, esmaltado", newStyle);
 			Servicio servicioPedicura = new Servicio("Pedicura", 500.0,  "url aqui", "url aqui","Limpieza y esmaltado", newStyle);
-
+			Servicio servicioPestaña = new Servicio("Extension Pestaña", 800.0,  "url aqui", "url aqui","Extenciones de pestañas", newStyle);
 
 
 			Cliente admin = new Cliente("Admin", "New Style", "admind@admin.com", passwordEncoder.encode("admin789"),"0000");
 			Cliente cliente1 = new Cliente("User", "Resu", "user@gmail.com", passwordEncoder.encode("user123"),"0000");
 			Cliente cliente2 = new Cliente("Martha", "Stuart Little", "msl@gmail.com", passwordEncoder.encode("user123"),"0000");
-			Cliente cliente3 = new Cliente("Pedro", "Gomez", "user@gmail.com", passwordEncoder.encode("user123"),"0000");
-			Cliente cliente4 = new Cliente("Mauro", "Perez", "user@gmail.com", passwordEncoder.encode("user123"),"0000");
+			Cliente cliente3 = new Cliente("Pedro", "Gomez", "pedro@gmail.com", passwordEncoder.encode("user123"),"0000");
+			Cliente cliente4 = new Cliente("Mauro", "Perez", "mauro@gmail.com", passwordEncoder.encode("user123"),"0000");
 
 
-			Producto productoEnjuague = new Producto("Enjuague para el cabello", 350.0, "url aqui", "url aqui", 8, "Recuperacion milagrosa para el cabello", newStyle);
-			Producto productoTinturaRoja = new Producto("Tintura Roja para el cabello", 350.0, "url aqui", "url aqui", 15, "Duracion extendida de 20 a 30 dias con lavado moderado agua tibia", newStyle);
-			Producto productoTinturaAzul = new Producto("Tintura Azul para el cabello", 350.0, "url aqui", "url aqui", 20, "Duracion extendida de 20 a 30 dias con lavado moderado agua tibia", newStyle);
+			Producto productoEnjuague = new Producto("Crema De Enjuague ", 350.0, "url aqui", "url aqui", 8, "Recuperacion milagrosa para el cabello", newStyle);
+			Producto productoTinturaRoja = new Producto("Tintura Roja ", 350.0, "url aqui", "url aqui", 15, "Duracion extendida de 20 a 30 dias con lavado moderado agua tibia", newStyle);
+			Producto productoTinturaAzul = new Producto("Tintura Azul ", 350.0, "url aqui", "url aqui", 20, "Duracion extendida de 20 a 30 dias con lavado moderado agua tibia", newStyle);
 			Producto productoKeratina = new Producto("Keratina", 500.0, "url aqui", "url aqui", 5, "Repara y nutre el cabello de raiz a las puntas", newStyle);
 			Producto productoBotoxCabello = new Producto("Botox Cabello", 600.0, "url aqui", "url aqui", 10, "Restaruracion y brillo del cabello", newStyle);
-			Producto productoAntiFrizz = new Producto("Anti Frizz", 800.0, "url aqui", "url aqui",4, "Controla el cabello con frizz con los productos de peinado en la ducha", newStyle);
+			Producto productoAntiFrizz = new Producto("Keratina Anti Frizz", 800.0, "url aqui", "url aqui",4, "Controla el cabello con frizz con los productos de peinado en la ducha", newStyle);
 
 			ClienteServicio clienteServicio1 = new ClienteServicio(cliente1, servicioAlisado);
 			ClienteProducto clienteProducto = new ClienteProducto(cliente1, productoBotoxCabello);
@@ -91,6 +91,7 @@ public class NewStyleApplication {
 			repositorioServicio.save(servicioTintura);
 			repositorioServicio.save(servicioManicura);
 			repositorioServicio.save(servicioPedicura);
+			repositorioServicio.save(servicioPestaña);
 
 			repositorioProducto.save(productoEnjuague);
 			repositorioProducto.save(productoTinturaRoja);
