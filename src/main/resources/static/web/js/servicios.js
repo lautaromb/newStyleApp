5aa04a75c8ca82c1a48b6f677c61d23c33b4e44f
Claudio_Pactat <claudio.pactat@gmail.com>
2022-03-16 11:19:51 -0300

    arreglado el inicio de admin para accesos

--- src/main/resources/static/web/js/servicios.js
+++ src/main/resources/static/web/js/servicios.js
@@ -2,12 +2,23 @@ var app = new Vue({
     el: "#app",
     data: {
       servicios: [],
+      rolAdmin: false,
     },
     created() {
+        this.cargarDatos();
         this.loadData();
     }
   ,
     methods: {
+      cargarDatos(){
+        axios.get('/api/cliente/current')
+        .then(response =>{
+          if(response.data.email.includes("@admin.com")){
+            this.rolAdmin = true;
+          }
+        })
+      },
+
         loadData: function () {
             axios.get("/api/servicio").then((response) => {
               console.log(response.data);
